28a718c64829caa6b7d4ee7ff6cae9986bdba764
estrellamjane <41849727+estrellamjane@users.noreply.github.com>
2018-07-29 16:35:17 +0800

    Update Connection_Detection.kt

--- app/src/main/java/com/erby/casestudy/Connection_Detection.kt
+++ app/src/main/java/com/erby/casestudy/Connection_Detection.kt
@@ -1,19 +1,19 @@
 
-package com.erby.casestudy <!-- name ng library package ay erby.casestudy -->
-import android.app.Service <!-- ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin-->
-import android.content.Context <!-- ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin-->
-import android.net.ConnectivityManager <!-- ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin-->
-import android.net.NetworkInfo <!-- ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin-->
+package com.erby.casestudy // name ng library package ay erby.casestudy 
+import android.app.Service // ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin
+import android.content.Context // ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin
+import android.net.ConnectivityManager // ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin
+import android.net.NetworkInfo // ito ay library package na kapag dineclare or tinawag, lahat ng attributes at classes ay pwede na din matawag at gamitin
 
-class Connection_Detection { <!-- the name of this class is Connection_Detection -->
-    var context: Context? = null <!-- in this class, nagdeclare tayo ng variable na ang pangalan ay 'context' -->
-    fun connect_detector(context:Context) { <!-- katumbas ng public void ang 'fun',kaya fun ang gamit dito dahil ang gamit na programming language ay Kotlin-->
-    <!-- Kotlin is a programming language use in Java Virtual Machine na maaaring gamitin sa paggawa ng android applications. 'Public' means that anyone can have an access. 'Void' means no returning value-->                                       
-        this.context = context <!-- ang 'this' parameter ay ginagamit upang ma-clarify natin kung anong object ang pinag uusapan -->
+class Connection_Detection { // the name of this class is Connection_Detection 
+    var context: Context? = null // in this class, nagdeclare tayo ng variable na ang pangalan ay 'context' 
+    fun connect_detector(context:Context) { // katumbas ng public void ang 'fun',kaya fun ang gamit dito dahil ang gamit na programming language ay Kotlin
+    // Kotlin is a programming language use in Java Virtual Machine na maaaring gamitin sa paggawa ng android applications. 'Public' means that anyone can have an access. 'Void' means no returning value                                       
+        this.context = context // ang 'this' parameter ay ginagamit upang ma-clarify natin kung anong object ang pinag uusapan
     }
-    fun isConnected() { <!--  -->
+    fun isConnected() { 
         val connectivityManager = context?.getSystemService(Service.CONNECTIVITY_SERVICE) as ConnectivityManager
-       <!--  -->
+       
     }
 
 }
