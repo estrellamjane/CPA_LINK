e9bef615d3168b735f29d26ee6c3e9ba54f12cb9
Michaela Jane S. Estrella <41849727+estrellamjane@users.noreply.github.com>
2018-08-05 15:57:02 +0800

    Update info.kt

--- app/src/main/java/com/erby/casestudy/info.kt
+++ app/src/main/java/com/erby/casestudy/info.kt
@@ -1,6 +1,6 @@
 package com.erby.casestudy
 
-
+//a package that holds all the variables for the users information
 class info(val id:String, val lastname:String , val firsname:String , val bAdd : String, val email1: String , val tinn: String , val pass:String)
 
 class info1(val lastname:String , val firsname:String , val businessAddress : String, val tinNumber: String )
